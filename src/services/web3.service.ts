b236082f4088dc6a6fcafd904de41141ede3f93b
mibzman <sam@hungerperks.com>
2018-03-26 11:39:13 -0400

    refactor

--- src/services/web3.service.ts
+++ src/services/web3.service.ts
@@ -12,10 +12,17 @@ declare var window: any;
 @Injectable()
 export class Web3Service {
 
-	public web3: any;
+	public Web3: any;
+  public Account: any;
+  public Accounts: any;
 
   constructor() { 
   	this.checkAndInstantiateWeb3();
+
+    this.GetAccounts().subscribe(accs => {
+      this.Accounts = accs;
+      this.Account = this.Accounts[0];
+    })
   }
 
   checkAndInstantiateWeb3 = () => {
@@ -25,13 +32,13 @@ export class Web3Service {
         'Using web3 detected from external source. If you find that your accounts don\'t appear or you have 0 MetaCoin, ensure you\'ve configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask'
       );
       // Use Mist/MetaMask's provider
-      this.web3 = new Web3(window.web3.currentProvider);
+      this.Web3 = new Web3(window.web3.currentProvider);
     } else {
       console.warn(
         'No web3 detected. Falling back to ${environment.HttpProvider}. You should remove this fallback when you deploy live, as it\'s inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask'
       );
       // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-      this.web3 = new Web3(
+      this.Web3 = new Web3(
         new Web3.providers.HttpProvider(environment.HttpProvider)
       );
     }
@@ -39,13 +46,12 @@ export class Web3Service {
 
   getAccounts(): Observable<any>{
   	return Observable.create(observer => {
-  	  this.web3.eth.getAccounts((err, accs) => {
+  	  this.Web3.eth.getAccounts((err, accs) => {
   	    if (err != null) {
+          if (accs.length === 0) {
+            observer.error('Couldn\'t get any accounts! Make sure your Ethereum client is configured correctly.')
+          }
   	      observer.error('There was an error fetching your accounts.')
-  	    }
-
-  	    if (accs.length === 0) {
-  	      observer.error('Couldn\'t get any accounts! Make sure your Ethereum client is configured correctly.')
   	    }
 
   	    observer.next(accs)
