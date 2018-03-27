6e716bb292ec38d14d5ec316cc389888b1e5ae51
mibzman <sam@hungerperks.com>
2018-03-26 20:37:13 -0400

    removed warning thing

--- src/services/web3.service.ts
+++ src/services/web3.service.ts
@@ -28,9 +28,9 @@ export class Web3Service {
   checkAndInstantiateWeb3 = () => {
     // Checking if Web3 has been injected by the browser (Mist/MetaMask)
     if (typeof window.web3 !== 'undefined') {
-      console.warn(
-        'Using web3 detected from external source. If you find that your accounts don\'t appear or you have 0 MetaCoin, ensure you\'ve configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask'
-      );
+      // console.warn(
+      //   'Using web3 detected from external source. If you find that your accounts don\'t appear or you have 0 MetaCoin, ensure you\'ve configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask'
+      // );
       // Use Mist/MetaMask's provider
       this.Web3 = new Web3(window.web3.currentProvider);
     } else {
